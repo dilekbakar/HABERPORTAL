4cd846a882270382b140157281a96d14e832f105
dilekbakar <dilekbakar22@gmail.com>
2022-07-29 13:44:40 +0300

    commit
    IRepository class created.

--- HaberPortal/HaberPortal.Core/Infrastructure/IRepository.cs
+++ HaberPortal/HaberPortal.Core/Infrastructure/IRepository.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HaberPortal.Core.Infrastructure
 {
-    public interface IRepository<T>
+    public interface IRepository<T> where T : class
     {
+        //bu bizim tüm datamızı çekecek.
+        IEnumerable<T> GetAll();
+
+        //tek bir nesne döndürür.
+        T GetById(int d);
+
+        //yine tek nesne döndürecek fakat expressionla.
+        T Get(Expression<Func<T, bool>> expression);
+
+        //birden fazla değer dönecek fakat expressionla.
+        IQueryable<T> GetMany(Expression<Func<T, bool>> expression);
+
+        void Insert(T obj);
+        void Update(T obj);
+
+        void Delete(int id);
+
+        int Count();
+        void Save();
+
+
+
+
     }
 }
